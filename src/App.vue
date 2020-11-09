e6b38ea052e2470fafc872df033f44ef62846ee5
yekRosyid <35427619+yekRosyid@users.noreply.github.com>
2020-11-09 21:21:29 +0700

    config
    Belajar awal

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>List Couses</h1>
+  <ul>
+    <li v-for="(course, index) in courses" v-bind:key="index">
+      {{ course.name }}
+    </li>
+  </ul>
+  <form
+    @submit.prevent="
+      courses.push({ name: newCourse });
+      newCourse = '';
+    "
+  >
+    <input v-model="newCourse" />
+    <button type="submit">new Course</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      newCourse: "",
+      courses: [
+        { name: "NodeJs" },
+        { name: "Vue" },
+        { name: "React" },
+        { name: "Svelte" },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
